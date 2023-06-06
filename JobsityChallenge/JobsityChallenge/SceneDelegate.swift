e88b3ce51cacc733f73868a74e9f10b455efaa6f
Paulo Ricardo <praraujo.vieira@gmail.com>
2023-06-06 14:21:09 -0300

    Initial commit

---
+++ JobsityChallenge/JobsityChallenge/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  JobsityChallenge
+//
+//  Created by Paulo Ricardo de Araujo Vieira on 06/06/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let navController = UINavigationController(rootViewController: ViewController())
+        window = .init(windowScene: scene)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ JobsityChallenge/JobsityChallenge/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  JobsityChallenge
+//
+//  Created by Paulo Ricardo de Araujo Vieira on 06/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+}
